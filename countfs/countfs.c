428ab6dc27e05d1f910928af9ba4e19f021aed63
Luis de Bethencourt <luisbg@osg.samsung.com>
2016-08-15 15:13:18 +0100

    countfs: rename module internally

--- countfs/countfs.c
+++ countfs/countfs.c
@@ -7,44 +7,44 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
 
-#define TOYFS_MAGIC 0x20160408
+#define COUNTFS_MAGIC 0x20160408
 #define TMPSIZE 12
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luis de Bethencourt");
 
-static struct dentry *toyfs_mount(struct file_system_type *fs_type,
+static struct dentry *countfs_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data);
-static struct dentry *toyfs_create_file(struct super_block *sb,
+static struct dentry *countfs_create_file(struct super_block *sb,
 		umode_t mode, const char *name);
-static int toyfs_user_create_file(struct inode *dir, struct dentry *dentry,
+static int countfs_user_create_file(struct inode *dir, struct dentry *dentry,
 		umode_t mode, bool excl);
-static ssize_t toyfs_read_file(struct file *filp, char *buf,
+static ssize_t countfs_read_file(struct file *filp, char *buf,
 		size_t count, loff_t *offset);
-static ssize_t toyfs_write_file(struct file *filp, const char *buf,
+static ssize_t countfs_write_file(struct file *filp, const char *buf,
 		size_t count, loff_t *offset);
 
-static struct file_system_type toyfs_type = {
+static struct file_system_type countfs_type = {
 	.owner		= THIS_MODULE,
-	.name		= "toyfs",
-	.mount		= toyfs_mount,
+	.name		= "countfs",
+	.mount		= countfs_mount,
 	.kill_sb	= kill_litter_super,
 	.fs_flags	= FS_USERNS_MOUNT,
 };
 
-static const struct super_operations toyfs_s_ops = {
+static const struct super_operations countfs_s_ops = {
 	.statfs		= simple_statfs,
 	.drop_inode	= generic_delete_inode,
 };
 
-static const struct file_operations toyfs_file_ops = {
+static const struct file_operations countfs_file_ops = {
 	.open		= simple_open,
-	.read		= toyfs_read_file,
-	.write		= toyfs_write_file,
+	.read		= countfs_read_file,
+	.write		= countfs_write_file,
 };
 
-static const struct inode_operations toyfs_dir_inode_ops = {
-	.create		= toyfs_user_create_file,
+static const struct inode_operations countfs_dir_inode_ops = {
+	.create		= countfs_user_create_file,
 	.lookup		= simple_lookup,
 	.link		= simple_link,
 	.unlink		= simple_unlink,
@@ -52,13 +52,13 @@ static const struct inode_operations toyfs_dir_inode_ops = {
 	.rename		= simple_rename,
 };
 
-static struct inode *toyfs_get_inode(struct super_block *sb,
+static struct inode *countfs_get_inode(struct super_block *sb,
 		const struct inode *dir, umode_t mode, dev_t dev)
 {
 	atomic_t *counter = NULL;
 	struct inode *ret = new_inode(sb);
 
-	pr_info("tfs: get_inode");
+	pr_info("cfs: get_inode");
 
 	if (ret) {
 		ret->i_mode = mode;
@@ -69,14 +69,14 @@ static struct inode *toyfs_get_inode(struct super_block *sb,
 		atomic_set(counter, 0);
 
 		if (mode & S_IFREG) {
-			pr_info("tfs: get_inode: creating a regular file");
+			pr_info("cfs: get_inode: creating a regular file");
 
-			ret->i_fop = &toyfs_file_ops;
+			ret->i_fop = &countfs_file_ops;
 			ret->i_private = counter;
 		} else {
-			pr_info("tfs: get_inode: creating a directory");
+			pr_info("cfs: get_inode: creating a directory");
 
-			ret->i_op = &toyfs_dir_inode_ops;
+			ret->i_op = &countfs_dir_inode_ops;
 			ret->i_fop = &simple_dir_operations;
 		}
 	}
@@ -84,38 +84,38 @@ static struct inode *toyfs_get_inode(struct super_block *sb,
 	return ret;
 }
 
-static int toyfs_fill_super(struct super_block *sb, void *data, int silent)
+static int countfs_fill_super(struct super_block *sb, void *data, int silent)
 {
 	static struct tree_descr toy_files[] = { {""} };
 	struct inode *inode;
 	int err;
 
-	pr_info("tfs: fill_super");
+	pr_info("cfs: fill_super");
 
-	err = simple_fill_super(sb, TOYFS_MAGIC, toy_files);
+	err = simple_fill_super(sb, COUNTFS_MAGIC, toy_files);
 	if (err)
 		return err;
 
-	sb->s_op = &toyfs_s_ops;
+	sb->s_op = &countfs_s_ops;
 
-	inode = toyfs_get_inode(sb, NULL, S_IFDIR, 0);
+	inode = countfs_get_inode(sb, NULL, S_IFDIR, 0);
 	sb->s_root = d_make_root(inode);
 	if (!sb->s_root)
 		return -ENOMEM;
 
-	toyfs_create_file(sb, S_IFREG, "counter");
+	countfs_create_file(sb, S_IFREG, "counter");
 
 	return 0;
 }
 
-static struct dentry *toyfs_create_file(struct super_block *sb,
+static struct dentry *countfs_create_file(struct super_block *sb,
 		umode_t mode, const char *name)
 {
 	struct dentry *dentry;
 	struct inode *inode;
 	struct qstr qname;
 
-	pr_info("tfs: create_file");
+	pr_info("cfs: create_file");
 
 	qname.name = name;
 	qname.len = strlen(name);
@@ -124,7 +124,7 @@ static struct dentry *toyfs_create_file(struct super_block *sb,
 	if (!dentry)
 		goto out;
 
-	inode = toyfs_get_inode(sb, NULL, mode | 0644, 0);
+	inode = countfs_get_inode(sb, NULL, mode | 0644, 0);
 	if (!inode)
 		goto out_dput;
 
@@ -137,14 +137,14 @@ out:
 	return 0;
 }
 
-static int toyfs_user_create_file(struct inode *dir, struct dentry *dentry,
+static int countfs_user_create_file(struct inode *dir, struct dentry *dentry,
 		umode_t mode, bool excl)
 {
 	struct inode *inode;
 
-	pr_info("tfs: create");
+	pr_info("cfs: create");
 
-	inode = toyfs_get_inode(dir->i_sb, dir, mode | S_IFREG, 0);
+	inode = countfs_get_inode(dir->i_sb, dir, mode | S_IFREG, 0);
 	if (!inode)
 		return -ENOSPC;
 
@@ -155,22 +155,22 @@ static int toyfs_user_create_file(struct inode *dir, struct dentry *dentry,
 	return 0;
 }
 
-static struct dentry *toyfs_mount(struct file_system_type *fs_type,
+static struct dentry *countfs_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data)
 {
-	pr_info("tfs: mount");
+	pr_info("cfs: mount");
 
-	return mount_nodev(fs_type, flags, data, toyfs_fill_super);
+	return mount_nodev(fs_type, flags, data, countfs_fill_super);
 }
 
-static ssize_t toyfs_read_file(struct file *filp, char *buf,
+static ssize_t countfs_read_file(struct file *filp, char *buf,
 		size_t count, loff_t *offset)
 {
 	int c, len;
 	char tmp[TMPSIZE];
 	atomic_t *counter = (atomic_t *) filp->private_data;
 
-	pr_info("tfs: read");
+	pr_info("cfs: read");
 
 	c = atomic_read(counter);
 	atomic_inc(counter);
@@ -179,7 +179,7 @@ static ssize_t toyfs_read_file(struct file *filp, char *buf,
 	return simple_read_from_buffer(buf, count, offset, tmp, len);
 }
 
-static ssize_t toyfs_write_file(struct file *filp, const char *buf,
+static ssize_t countfs_write_file(struct file *filp, const char *buf,
 		size_t count, loff_t *offset)
 {
 	char tmp[TMPSIZE];
@@ -187,30 +187,30 @@ static ssize_t toyfs_write_file(struct file *filp, const char *buf,
 	long val;
 	int err;
 
-	pr_info("tfs: write");
+	pr_info("cfs: write");
 
 	simple_write_to_buffer(tmp, TMPSIZE, offset, buf, count);
 	tmp[count] = '\0';    /* ktrstrol needs a null terminated string */
 	err = kstrtol(tmp, 10, &val);
 	if (!err) {
 		atomic_set(counter, val);
-		pr_info("tfs: count is now %ld", val);
+		pr_info("cfs: count is now %ld", val);
 	}
 
 	return count;
 }
 
-static int __init init_toyfs(void)
+static int __init init_countfs(void)
 {
-	pr_info("Loading toyfs\n");
-	return register_filesystem(&toyfs_type);
+	pr_info("Loading countfs\n");
+	return register_filesystem(&countfs_type);
 }
 
-static void __exit exit_toyfs(void)
+static void __exit exit_countfs(void)
 {
-	pr_info("Closing toyfs\n");
-	unregister_filesystem(&toyfs_type);
+	pr_info("Closing countfs\n");
+	unregister_filesystem(&countfs_type);
 }
 
-module_init(init_toyfs);
-module_exit(exit_toyfs);
+module_init(init_countfs);
+module_exit(exit_countfs);
